83a87cc44833fde0789f6137d18b198075cb53ab
haru48 <haru48program@gmail.com>
2022-04-19 23:45:31 +0900

    Create index.html

--- camera.py
+++ camera.py
@@ -1,27 +1,13 @@
 import cv2
 import streamlit as st
 st.title("QRコード読みとり")
-option = st.selectbox(
-     '入室　退室',
-     ('入室', '退室'))
 FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
-inroom = set()
-outroom = set()
-while option == "入室": 
-    _, frame = camera.read()
+while True:
+    ret, frame = cap.read()
     data = detector.detectAndDecode(frame)
     if data[0] != "":
-        inroom.add(data[0])
-        st.write(data[0])
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-while option == "退室": 
-    _, frame = camera.read()
-    data = detector.detectAndDecode(frame)
-    if data[0] != "":
-        outroom.add(data[0])
         st.write(data[0])
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
