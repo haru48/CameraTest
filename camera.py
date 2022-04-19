7294e3e5969b4e297eab9d99a1b33f414e166e34
haru48 <haru48program@gmail.com>
2022-04-20 00:40:09 +0900

    Create index.html

--- camera.py
+++ camera.py
@@ -6,9 +6,8 @@ cap = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
 while True:
     ret, frame = cap.read()
-    data = detector.detectAndDecode(frame)
-    if data[0] != "":
-        st.write(data[0])
+    # data = detector.detectAndDecode(frame)
+    # if data[0] != "":
+    #     st.write(data[0])
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
-
