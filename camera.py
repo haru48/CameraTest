3b342e708f652436fb789d9620055c9c2b2a73c9
haru48 <haru48program@gmail.com>
2022-04-20 17:57:35 +0900

    Create index.html

--- camera.py
+++ camera.py
@@ -6,17 +6,17 @@ st.title("QRコード読みとり")
 FRAME_WINDOW = st.image([])
 cap = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
-# class VideoProcessor:
-#     def recv(self, frame):
-#         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         data = detector.detectAndDecode(frame)
-#         if data[0] != "":
-#             st.write(data[0])
-# webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
-while True:
-    ret, frame = cap.read()
-    data = detector.detectAndDecode(frame)
-    if data[0] != "":
-        st.write(data[0])
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # FRAME_WINDOW.image(frame)
+class VideoProcessor:
+    def recv(self, frame):
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        data = detector.detectAndDecode(frame)
+        if data[0] != "":
+            st.write(data[0])
+webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+# while True:
+#     ret, frame = cap.read()
+#     data = detector.detectAndDecode(frame)
+#     if data[0] != "":
+#         st.write(data[0])
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     FRAME_WINDOW.image(frame)
